214783f7e42e2f69a2ced17555d12249cbc9e994
Marcos L <marcoslopez1895@gmail.com>
2023-06-20 15:33:55 -0400

    acomodando componentes del formcomponent

--- src/components/global/FormComponent.vue
+++ src/components/global/FormComponent.vue
@@ -12,7 +12,18 @@
               :rules="field.rules"
               :label="field.label"
             />
-            <VSelect
+            <SelectComponent
+              v-else-if="field.type === 'select'"
+              :items="(field.select?.items.value as any[])"
+              v-model="form[field.valueForm]"
+              :name="field.label"
+              :item-title="field.select!.itemTitle"
+              :item-value="field.select!.itemValue"
+              :rules="field.rules"
+              :menu-props="field.select!.menuProps"
+              :multiple="field.select!.multiple"
+            />
+            <!-- <VSelect
               v-else-if="field.type === 'select'"
               :items="field.select?.items.value"
               v-model="form[field.valueForm]"
@@ -22,25 +33,24 @@
               :rules="field.rules"
               :menu-props="field.select!.menuProps"
               :multiple="field.select!.multiple"
-            />
-            <!-- <InputComponent 
-              v-else
-
-              v-model="form[field.valueForm]"
-              :type="field.type"
-              :class="field.fieldClass"
-              :rules="field.rules"
-              :label="field.label"
-              :id="field.valueForm"
             /> -->
-            <VTextField
+            <InputComponent 
+              v-else
+              v-model="form[field.valueForm]"
+              :type="field.type"
+              :class="field.fieldClass"
+              :rules="field.rules"
+              :name="field.label"
+              :id="field.valueForm"
+            />
+            <!-- <VTextField
               v-else
               v-model="form[field.valueForm]"
               :type="field.type"
               :class="field.fieldClass"
               :rules="field.rules"
               :label="field.label"
-            />
+            /> -->
           </VCol>
         </slot>
       </VRow>
@@ -51,6 +61,7 @@
 import { helperStore } from '@/helper'
 import type { Row } from '@/interfaces/FormComponent.helper'
 import InputComponent from '@/components/InputComponent.vue'
+import SelectComponent from '../SelectComponent.vue';
 import { storeToRefs } from 'pinia';
 
 const props = defineProps<Props>()
