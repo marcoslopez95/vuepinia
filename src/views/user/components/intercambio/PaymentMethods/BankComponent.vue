133676cd224b0b29bb7da4186b2fda87313578e3
Marcos L <marcoslopez1895@gmail.com>
2023-07-09 15:47:55 -0400

    init

--- src/views/user/components/intercambio/PaymentMethods/BankComponent.vue
+++ src/views/user/components/intercambio/PaymentMethods/BankComponent.vue
@@ -1,17 +1,46 @@
 <template>
     <div>
-Esto es un pago por banco
-{{ itemsDetails }}
+        <VRow>
+            <VCol cols="4" v-for="bankAccount, i in (itemsDetails as BankAccount[])" :key="i"
+                @click="selectItem(bankAccount)">
+                <VBtnDegree rounded="lg" :class="{
+                    'payment-selected': bankAccount.id == modelValue?.id
+                }" color="primary">
+                   
+                    {{ bankAccount.relationships?.bank.attributes.name }}
+                </VBtnDegree>
+            </VCol>
+        </VRow>
     </div>
 </template>
 
 <script setup lang="ts">
 import type { BankAccount } from '@/interfaces/CompanyAccount/BankAccount/BankAccount.model';
-defineProps<{
+import { ref } from 'vue';
+const { modelValue } = defineProps<{
     itemsDetails: any
+    modelValue: any
 }>()
+
+const emits = defineEmits<{
+    (e: 'update:model-value', value: BankAccount): void
+}>()
+
+const selectItem = (item: BankAccount) => {
+    emits('update:model-value', item)
+    itemSelect.value = item
+}
+const itemSelect = ref<BankAccount | null>(null)
+
 </script>
 
-<style scoped>
 
+<style scoped>
+.v-text-field__slot.payment-selected {
+    background: none;
+}
+
+.payment-selected {
+    color: white !important;
+}
 </style>
--- src/views/user/components/intercambio/PaymentMethods/OtherComponent.vue
+++ src/views/user/components/intercambio/PaymentMethods/OtherComponent.vue
@@ -1,17 +1,46 @@
 <template>
     <div>
-Esto es un pago por otro
-{{ itemsDetails }}
+        <VRow>
+            <VCol cols="4" v-for="OtherAccount, i in (itemsDetails as OtherAccount[])" :key="i"
+                @click="selectItem(OtherAccount)">
+                <VBtnDegree rounded="lg" :class="{
+                    'payment-selected': OtherAccount.id == modelValue?.id
+                }" color="primary">
+                   
+                    {{ OtherAccount.attributes.description }}
+                </VBtnDegree>
+            </VCol>
+        </VRow>
     </div>
 </template>
 
 <script setup lang="ts">
 import type { OtherAccount } from '@/interfaces/CompanyAccount/OtherAccount/OtherAccount.model';
-defineProps<{
+import { ref } from 'vue';
+const { modelValue } = defineProps<{
     itemsDetails: any
+    modelValue: any
 }>()
+
+const emits = defineEmits<{
+    (e: 'update:model-value', value: OtherAccount): void
+}>()
+
+const selectItem = (item: OtherAccount) => {
+    emits('update:model-value', item)
+    itemSelect.value = item
+}
+const itemSelect = ref<OtherAccount | null>(null)
+
 </script>
 
-<style scoped>
 
+<style scoped>
+.v-text-field__slot.payment-selected {
+    background: none;
+}
+
+.payment-selected {
+    color: white !important;
+}
 </style>
