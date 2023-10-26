4639761878fd5616da652a49e2f403b6c10a68ad
marcoslopez95 <50760667+marcoslopez95@users.noreply.github.com>
2023-10-26 10:15:33 -0400

    ts

--- src/interfaces/TransactionXCOP/TransactionXCOP.dto.ts
+++ src/interfaces/TransactionXCOP/TransactionXCOP.dto.ts
@@ -1,8 +1,9 @@
 import type { TransactionXCOPAttributes } from "./TransactionXCOP.model"
 
-export declare interface TransactionXCOPCreate extends create {
+export declare interface TransactionXCOPCreate  {
     xcop_send: number | string
+    user_receipt_id: number | string
 }
 
-type pickCreate = 'user_receipt_id'
-type create = Pick<TransactionXCOPAttributes,pickCreate>
+// type pickCreate = 'user_receipt_id'
+// type create = Pick<TransactionXCOPAttributes,pickCreate>
