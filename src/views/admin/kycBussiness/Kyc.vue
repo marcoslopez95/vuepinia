4ca111a2ead58ea16eac58f9e3525f9514e78d82
Guillermo Berbesi <dev.guillermoberbesi@gmail.com>
2024-01-03 18:36:45 -0400

    init

--- src/views/admin/kycBussiness/Kyc.vue
+++ src/views/admin/kycBussiness/Kyc.vue
@@ -1,90 +1,97 @@
 <template>
-    <div v-if="helper.clickIn !='Show'">
+    <div v-if="helper.clickIn != 'Show'">
         <div class="d-flex justify-space-between mx-5 mb-5">
-            <h2 class="text-primary font-weight-bold">{{ $t('views.admin.kyc.title') }} Empresas</h2>
+            <h2 class="text-primary font-weight-bold">
+                {{ $t("views.admin.kyc.title") }} Empresas
+            </h2>
             <SearchInputComponentVue v-model="search" @onSearch="getSearch" />
         </div>
 
-        <TableComponentVue 
-            optionsHabilit 
-            icon-show 
-            :headers="headers" 
-            @show="showItem" 
+        <TableComponentVue
+            optionsHabilit
+            icon-show
+            :headers="headers"
+            @show="showItem"
             :items="helper.items"
         >
-        <template #cel-attributes.status="{data}">
-            <VChipSuccess
-                v-if="(data as Kyc).attributes.status === KYC_STATUS.ACCEPT"
+            <template #cel-relationships.kyc.attributes.status="{ data }">
+                <VChipSuccess
+                    v-if="getStatusKyc(bussiness(data)) === KYC_STATUS.ACCEPT"
                 >
-                {{(data as Kyc).attributes.status}}
-            </VChipSuccess>
-            <VChipError
-                v-if="(data as Kyc).attributes.status === KYC_STATUS.REJECT"
+                    {{ getStatusKyc(bussiness(data)) }}
+                </VChipSuccess>
+                <VChipError
+                    v-if="getStatusKyc(bussiness(data)) === KYC_STATUS.REJECT"
                 >
-                {{(data as Kyc).attributes.status}}
-            </VChipError>
-            <VChipWarning
-                v-if="(data as Kyc).attributes.status === KYC_STATUS.IN_VERIFICATION"
+                    {{ getStatusKyc(bussiness(data)) }}
+                </VChipError>
+                <VChipWarning
+                    v-if="
+                        getStatusKyc(bussiness(data)) ===
+                        KYC_STATUS.IN_VERIFICATION
+                    "
                 >
-                {{(data as Kyc).attributes.status}}
-            </VChipWarning>
-        </template>
+                    {{ getStatusKyc(bussiness(data)) }}
+                </VChipWarning>
+            </template>
         </TableComponentVue>
     </div>
-    <KycShow 
-        v-else
-        @back="helper.index"
-        />
+    <KycShow v-else @back="helper.index" />
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import { useI18n } from 'vue-i18n';
-import { helperStore } from '@/helper';
-import SearchInputComponentVue from '@/components/global/SearchInputComponent.vue';
-import TableComponentVue from '@/components/global/TableComponent.vue';
-import type { Head } from '@/interfaces/TableComponent.helper';
-import KycShow from './KycShow.vue';
-import type { Kyc } from '@/interfaces/User/Kyc/Kyc.model';
-import { KYC_STATUS } from '@/enums/Kyc.enum';
+import { ref } from "vue";
+import { useI18n } from "vue-i18n";
+import { helperStore } from "@/helper";
+import SearchInputComponentVue from "@/components/global/SearchInputComponent.vue";
+import TableComponentVue from "@/components/global/TableComponent.vue";
+import type { Head } from "@/interfaces/TableComponent.helper";
+import KycShow from "./KycShow.vue";
+import type { Kyc } from "@/interfaces/User/Kyc/Kyc.model";
+import { KYC_STATUS } from "@/enums/Kyc.enum";
+import type { Bussiness } from "@/interfaces/Bussiness/Bussiness.model";
 
-const showKyc = ref(false)
-const { t } = useI18n()
-const helper = helperStore()
-const search = ref<string>('')
-helper.url = 'kyc'
+const showKyc = ref(false);
+const { t } = useI18n();
+const helper = helperStore();
+const search = ref<string>("");
+helper.url = "companys";
 
-helper.index()
+helper.index();
 
+const bussiness = (item: unknown) => item as Bussiness;
+
+const getStatusKyc = (item: Bussiness): string =>
+    item.relationships!.kyc?.attributes.status ?? "";
 const getSearch = () => {
     helper.index({
-        type_document: search.value
-    })
-}
+        type_document: search.value,
+    });
+};
 
 const headers: Head[] = [
     {
-        name: t('general-views.id'),
-        value: 'id',
+        name: t("general-views.id"),
+        value: "id",
     },
     {
-        name: t('views.users.title'),
-        value: 'relationships.user.attributes.username',
+        name: t("views.users.title"),
+        value: "relationships.user.attributes.username",
     },
     {
-        name: t('views.users.email'),
-        value: 'relationships.user.attributes.email',
+        name: t("views.users.email"),
+        value: "relationships.user.attributes.email",
     },
     {
-        name: t('general-views.status'),
-        value: 'attributes.status',
+        name: t("general-views.status"),
+        value: "relationships.kyc.attributes.status",
     },
-]
+];
 
-const showItem = (element: Kyc) => {
-    helper.item = element
-    helper.clickIn = 'Show'
-}
+const showItem = (element: Bussiness) => {
+    helper.item = element;
+    helper.clickIn = "Show";
+};
 </script>
 
 <style scoped></style>
