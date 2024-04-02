9663a9852d21ac4fbc3d321cbd95a01b8b62e552
Guillermo Berbesi <dev.guillermoberbesi@gmail.com>
2024-04-02 13:18:20 -0400

    agregando proteccion completa

--- src/views/auth/Register.vue
+++ src/views/auth/Register.vue
@@ -1,116 +1,163 @@
-
 <script setup lang="ts">
 import { ref, watch } from "vue";
 import InputComponent from "@/components/InputComponent.vue";
 import { helperStore } from "@/helper";
-import * as validator from '@/validator'
+import * as validator from "@/validator";
 import { storeToRefs } from "pinia";
 import { useRoute, useRouter } from "vue-router";
 import { toast } from "vue3-toastify";
 import { useI18n } from "vue-i18n";
-import { RegisterStore } from '@/stores/RegisterStore'
-import TelInput from '@/components/TelInput.vue';
+import { RegisterStore } from "@/stores/RegisterStore";
+import TelInput from "@/components/TelInput.vue";
 import { computed } from "vue";
+// import "https://www.google.com/recaptcha/api.js";
+// import { useRecaptchaProvider } from 'vue-recaptcha/dist/head'
 
-const { t } = useI18n()
-const helper = helperStore()
-const router = useRouter()
-const { formRef } = storeToRefs(helper)
-const registerStore = RegisterStore()
-const { form } = storeToRefs(registerStore)
-const isPhoneValid = ref(false)
-const phoneFormat = ref('')
-const accepTerms = ref(false)
+// useRecaptchaProvider()
 
-const removeSpace = (event:any) => {
-    if(event.key == ' '){
-        event.preventDefault()
-        return
+const { t } = useI18n();
+const helper = helperStore();
+const router = useRouter();
+const { formRef } = storeToRefs(helper);
+const registerStore = RegisterStore();
+const { form } = storeToRefs(registerStore);
+const isPhoneValid = ref(false);
+const phoneFormat = ref("");
+const accepTerms = ref(false);
+const response = ref();
+const removeSpace = (event: any) => {
+    if (event.key == " ") {
+        event.preventDefault();
+        return;
     }
-}
+};
 
-const route = useRoute()
-const { email } = route.query
-if(email){
-    form.value.email = email as string
+const route = useRoute();
+const { email } = route.query;
+if (email) {
+    form.value.email = email as string;
 }
-const showPassword = ref(false)
-const showPasswordConfirmation = ref(false)
+const showPassword = ref(false);
+const showPasswordConfirmation = ref(false);
 
 const SigUp = async () => {
-    const { valid } = await formRef.value?.validate()
+    const { valid } = await formRef.value?.validate();
     if (!valid) {
-        return
+        return;
     }
-    form.value.referred_by = route.query.ref as string
-    helper
-        .http('register', 'post', { data: form.value })
-        .then((res) => {
-            toast.success(t('views.register.success'))
-            router.push({ name: 'Login' })
-        })
-}
+    let tokenGoogle = ''
+    // @ts-ignore
+    grecaptcha.enterprise.ready(async () => {
+        // @ts-ignore
+        tokenGoogle = await grecaptcha.enterprise.execute(
+            "6Le8v6spAAAAAOKosidEjezeVNyvZuxR-mjypbSE",
+            { action: "LOGIN" }
+        );
+        // console.log("token,google", token);
+    });
+    form.value.referred_by = route.query.ref as string;
+    const data = {
+        ...form.value,
+        tokenGoogle
+    }
+    helper.http("register", "post", { data }).then((res) => {
+        toast.success(t("views.register.success"));
+        router.push({ name: "Login" });
+    });
+};
 
 const eventOnlyLetter = {
-    keypress : validator.keyPressOnlyLettersAndSpace
+    keypress: validator.keyPressOnlyLettersAndSpace,
+};
+function onClick(e) {
+    e.preventDefault();
 }
 </script>
 
 <template>
     <v-toolbar color="transparent" class="text-center">
         <v-toolbar-title class="font-weight-light text-primary">
-            {{ $t('views.register.title') }}
+            {{ $t("views.register.title") }}
         </v-toolbar-title>
     </v-toolbar>
     <div class="">
-        <VForm ref="formRef" class="mx-3" style="min-height: 212px;"
-        @submit.prevent="()=>{}"
+        <VForm
+            ref="formRef"
+            class="mx-3"
+            style="min-height: 212px"
+            @submit.prevent="() => {}"
         >
             <template #default="{ isValid }">
                 <VRow>
                     <VCol cols="12">
-                        <InputComponent 
-                            :placeholder="$t('views.users.username')" withoutLabel :name="$t('views.users.username')" v-model="form.username"
-                            :rules="[validator.required]" 
-                            :events="{keypress: removeSpace}"/>
+                        <InputComponent
+                            :placeholder="$t('views.users.username')"
+                            withoutLabel
+                            :name="$t('views.users.username')"
+                            v-model="form.username"
+                            :rules="[validator.required]"
+                            :events="{ keypress: removeSpace }"
+                        />
                     </VCol>
                     <VCol cols="12">
-                        <InputComponent :events="eventOnlyLetter" :placeholder="$t('views.users.first_name')" withoutLabel :name="$t('views.users.first_name')" v-model="form.first_name"
-                            :rules="[validator.required]" />
+                        <InputComponent
+                            :events="eventOnlyLetter"
+                            :placeholder="$t('views.users.first_name')"
+                            withoutLabel
+                            :name="$t('views.users.first_name')"
+                            v-model="form.first_name"
+                            :rules="[validator.required]"
+                        />
                     </VCol>
                     <VCol cols="12">
-                        <InputComponent :events="eventOnlyLetter" :placeholder="$t('views.users.last_name')" withoutLabel :name="$t('views.users.last_name')" v-model="form.last_name"
-                            :rules="[validator.required]" />
+                        <InputComponent
+                            :events="eventOnlyLetter"
+                            :placeholder="$t('views.users.last_name')"
+                            withoutLabel
+                            :name="$t('views.users.last_name')"
+                            v-model="form.last_name"
+                            :rules="[validator.required]"
+                        />
                     </VCol>
                     <VCol cols="12">
-                        <InputComponent :placeholder="$t('views.users.email')" withoutLabel :name="$t('views.users.email')" v-model="form.email"
-                            :rules="[validator.required, validator.email]" />
+                        <InputComponent
+                            :placeholder="$t('views.users.email')"
+                            withoutLabel
+                            :name="$t('views.users.email')"
+                            v-model="form.email"
+                            :rules="[validator.required, validator.email]"
+                        />
                     </VCol>
                     <VCol cols="12">
-                        <TelInput 
-                            v-model="phoneFormat" 
-                            @phone="(phone) => form.phone = phone"
-                            @phoneCode="(code) => form.code_phone = code" :name="$t('views.profile.personal-data.phone')"
-                            required @isValid="(bool) => isPhoneValid = bool"
-                            :placeholder="$t('views.profile.personal-data.phone')"
+                        <TelInput
+                            v-model="phoneFormat"
+                            @phone="(phone) => (form.phone = phone)"
+                            @phoneCode="(code) => (form.code_phone = code)"
+                            :name="$t('views.profile.personal-data.phone')"
+                            required
+                            @isValid="(bool) => (isPhoneValid = bool)"
+                            :placeholder="
+                                $t('views.profile.personal-data.phone')
+                            "
                             without-label
-                            />
+                        />
                     </VCol>
                     <VCol cols="12">
-                        
-                        <InputComponent 
+                        <InputComponent
                             :name="$t('views.users.password')"
                             withoutLabel
                             :placeholder="$t('views.users.password')"
                             v-model="form.password"
-                            :rules="[validator.required,validator.password]"
-                            :type="!showPassword? 'password' : 'text'"
-                            :appendIcon="!showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                            :rules="[validator.required, validator.password]"
+                            :type="!showPassword ? 'password' : 'text'"
+                            :appendIcon="
+                                !showPassword ? 'mdi-eye' : 'mdi-eye-off'
+                            "
                             @click:append-icon="showPassword = !showPassword"
-                            />
-                            <br>
+                        />
+                        <br />
                         <span class="text-table">
-                            La contraseña debe cumplir con:<br>
+                            La contraseña debe cumplir con:<br />
                             <ul class="ml-8">
                                 <li>1 mayúscula</li>
                                 <li>1 caractér especial (./_*?¿)</li>
@@ -120,31 +167,51 @@ const eventOnlyLetter = {
                         </span>
                     </VCol>
                     <VCol cols="12">
-                        <InputComponent 
+                        <InputComponent
                             withoutLabel
-                            :placeholder="$t('views.users.password-confirmation')"
-                            :name="$t('views.users.password-confirmation')" 
+                            :placeholder="
+                                $t('views.users.password-confirmation')
+                            "
+                            :name="$t('views.users.password-confirmation')"
                             v-model="form.password_confirmation"
                             :rules="[
                                 validator.required,
                                 validator.password,
-                                validator.confirmPassword(form.password_confirmation, form.password)
+                                validator.confirmPassword(
+                                    form.password_confirmation,
+                                    form.password
+                                ),
                             ]"
-                            :type="!showPasswordConfirmation? 'password' : 'text'"
-                            :appendIcon="!showPasswordConfirmation ? 'mdi-eye' : 'mdi-eye-off'"
-                            @click:append-icon="showPasswordConfirmation = !showPasswordConfirmation"
-                            />
+                            :type="
+                                !showPasswordConfirmation ? 'password' : 'text'
+                            "
+                            :appendIcon="
+                                !showPasswordConfirmation
+                                    ? 'mdi-eye'
+                                    : 'mdi-eye-off'
+                            "
+                            @click:append-icon="
+                                showPasswordConfirmation =
+                                    !showPasswordConfirmation
+                            "
+                        />
                     </VCol>
                 </VRow>
                 <VCheckbox v-model="accepTerms">
                     <template #label>
-                        <div v-html="$t('views.register.accept-terms-and-conditions')">
-                        </div>
+                        <div
+                            v-html="
+                                $t('views.register.accept-terms-and-conditions')
+                            "
+                        ></div>
                     </template>
                 </VCheckbox>
                 <div class="text-center mt-1">
-                    <VBtnPrimary @click="SigUp" :disabled="!accepTerms || (!isValid.value)">
-                        {{ $t('views.register.button') }} 
+                    <VBtnPrimary
+                        @click="SigUp"
+                        :disabled="!accepTerms || !isValid.value"
+                    >
+                        {{ $t("views.register.button") }}
                     </VBtnPrimary>
                 </div>
             </template>
@@ -152,13 +219,15 @@ const eventOnlyLetter = {
     </div>
     <VRow>
         <VCol class="text-center text-table mt-3">
-            {{ $t('views.register.you-have-an-account') }}
-            <RouterLink :to="{ name: 'Login' }" class="text-primary font-weight-bold" style="text-decoration: none;">
-                {{ $t('views.register.login') }}
+            {{ $t("views.register.you-have-an-account") }}
+            <RouterLink
+                :to="{ name: 'Login' }"
+                class="text-primary font-weight-bold"
+                style="text-decoration: none"
+            >
+                {{ $t("views.register.login") }}
             </RouterLink>
         </VCol>
     </VRow>
 </template>
-<style >
-
-</style>
+<style></style>
